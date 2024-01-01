37e8e0b99ce8c5d0b8bd36f69f6905ca29987981
Abdul B. Issa <abdulbadi.issa@gmail.com>
2023-12-31 23:50:16 -0600

    update build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 const cors = require('cors')
 const Note = require('./models/note')
 
