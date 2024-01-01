741acebfa81d70772b7245718e84981a8f3288c9
Abdul B. Issa <abdulbadi.issa@gmail.com>
2023-12-31 23:16:41 -0600

    connect backend to databse

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
+
+
+
+
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
     console.log("method: ", request.method)
@@ -38,7 +43,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -93,7 +100,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
