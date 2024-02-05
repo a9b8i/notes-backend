5bea2f42bb70ed8df4cea0fd80949ba75e749ec5
Abdul B. Issa <abdulbadi.issa@gmail.com>
2024-02-04 22:13:23 -0600

    part3d update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-app.use(express.static('build'))
 const cors = require('cors')
 const Note = require('./models/note')
 
@@ -21,23 +20,24 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
+
 let notes = [
     {
-      id: 1,
-      content: "HTML is easy",
-      important: true
+        id: 1,
+        content: "HTML is easy",
+        important: true
     },
     {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
     }
-  ]
+]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -51,32 +51,32 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        if(note){
+        if (note) {
             response.json(note)
         } else {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        return next(error)
-    })
-    
+        .catch(error => {
+            return next(error)
+        })
+
 })
 
 const generateId = () => {
-    const maxId = notes.length>0 ? Math.max(...notes.map(n => n.id)) : 0
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
     return maxId + 1;
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
-    if (!body.content){
+    if (!body.content) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
@@ -85,17 +85,15 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const {content, important} = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id,
+         {content, important},
+        { new: true, runValidators: true, context: query })
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -105,16 +103,16 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => {
-        return next(error)
-    })
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            return next(error)
+        })
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unkown path'})
+    response.status(404).send({ error: 'unkown path' })
 }
 
 app.use(unknownEndpoint)
@@ -123,13 +121,13 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
+        return response.status(400).send({ error: 'malformatted id' })
+    }else if (error.name === "ValidationError"){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
 }
-
-
 
 app.use(errorHandler)
 
