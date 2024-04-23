08df4fca054f6c6708a696711f6dfb3864bdf311
Abdul B. Issa <abdulbadi.issa@gmail.com>
2024-04-23 14:12:39 -0500

    populate done

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('notes', { content: 1, importnat: 1 })
   response.json(users)
 })
 
@@ -13,7 +13,7 @@ usersRouter.post('/', async (request, response) => {
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User ({
+  const user = new User({
     username,
     name,
     passwordHash,
